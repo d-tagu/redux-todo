0149da0b88a096f87d5ceb7cd91c8a538a40e1ef
daikitag01@gmail.com <d.taguchi@i3design.co.jp>
2021-08-09 19:29:07 +0900

    Add: project

---
+++ redux-todo/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import TaskList from "./components/TaskList";
+import TaskInput from "./components/TaskInput";
+import { Task } from "./types";
+import "./App.css";
+
+const initialState: Task[] = [
+  {
+    id: 2,
+    title: "次のTodo",
+    done: false,
+  },
+  {
+    id: 1,
+    title: "最初のTodo",
+    done: true,
+  },
+];
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState(initialState);
+
+  return (
+    <div>
+      <TaskInput tasks={tasks} setTasks={setTasks} />
+      <TaskList tasks={tasks} />
+    </div>
+  );
+};
+
+export default App;
